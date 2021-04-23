5ac4c94e361401056ac3759341f9b4e766221143
Philippe Raclet <praclet.tel@gmail.com>
2021-04-23 10:42:30 +0200

    Finishing ex01

--- ex01/srcs/Data.cpp
+++ ex01/srcs/Data.cpp
@@ -6,19 +6,38 @@
 /*   By: praclet <praclet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/23 09:31:30 by praclet           #+#    #+#             */
-/*   Updated: 2021/04/23 09:47:45 by praclet          ###   ########lyon.fr   */
+/*   Updated: 2021/04/23 10:35:24 by praclet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cstdlib>
 #include "Data.hpp"
 
-void * serialize(void)
+static void fillString(char * str, std::size_t size)
 {
-	;
+	if (!str || !size)
+		return ;
+	for (std::size_t i = 0;i < size - 1;i++)
+		str[i] = rand() % (126 - 32 + 1) + 32;
+	str[size - 1] = 0;
 }
 
-Data * deserialize(void * raw);
+void * serialize(void)
+{
+	Data * res = new Data;
+	std::size_t size;
+
+	size = rand() % 25 + 10;
+	res->rndString1 = new char[size];
+	fillString(res->rndString1, size); 
+	res->rndInteger = rand() - RAND_MAX / 2;
+	size = rand() % 25 + 10;
+	res->rndString2 = new char[size];
+	fillString(res->rndString2, size); 
+	return (reinterpret_cast<void *>(res));
+}
+
+Data * deserialize(void * raw)
 {
 	return (reinterpret_cast<Data *>(raw));
 }
--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -6,16 +6,29 @@
 /*   By: praclet <praclet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/23 09:25:48 by praclet           #+#    #+#             */
-/*   Updated: 2021/04/23 09:47:49 by praclet          ###   ########lyon.fr   */
+/*   Updated: 2021/04/23 10:40:41 by praclet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
 #include "Data.hpp"
 
 int main(void)
 {
+	srand (time(NULL));
+	for (std::size_t i = 0;i < 10;i++)
+	{
+		Data *tmp=deserialize(serialize());
 
+		std::cout << "str1: '" << tmp->rndString1 << "'" << std::endl;
+		std::cout << " int: '" << tmp->rndInteger << "'" << std::endl;
+		std::cout << "str2: '" << tmp->rndString2 << "'" << std::endl;
+		std::cout << std::endl;
+		delete [] tmp->rndString1;
+		delete [] tmp->rndString2;
+		delete tmp;
+	}
 	return (0);
 }
